359dbe47432e2373fdde2fa0e7625f170f29d591
Mohamad Chebli <chebli17c@gmail.com>
2021-10-22 19:56:31 +0300

    Done with works section + slider

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
+import { useState, useEffect } from "react";
 import PortfolioList from "../PortfolioList/PortfolioList";
 import React from "./Portfolio.scss";
-import { useState } from "react";
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+    designPortfolio,
+    contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
-
     const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
     const list = [
         {
             id: "featured",
@@ -23,9 +30,32 @@ export default function Portfolio() {
             title: "Design"
         },
     ];
+
+    useEffect(() => {
+
+        switch (selected) {
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "design":
+                setData(designPortfolio);
+                break;
+            case "content":
+                setData(contentPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+
+    }, [selected]);
+
     return (
-
-
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
@@ -40,37 +70,16 @@ export default function Portfolio() {
 
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-
-
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="https://www.doublemesh.com/wp-content/uploads/2014/05/free-golden-web-template.jpg" alt=""></img>
-                    <h3>Banking App</h3>
-                </div>
-
+                {data.map((d) => (
+                    <div className="item">
+                        <img
+                            src={d.img}
+                            alt=""
+                        />
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
             </div>
-
-
         </div>
     );
 }
