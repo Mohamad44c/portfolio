3a023fa6a51bd0b3f436186d1d40e3f35ac87385
Mohamad Chebli <mohamed.chebli@lau.edu>
2022-01-18 22:53:02 +0200

    UI Changes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,7 +8,6 @@ export default function Experience() {
             title: "Senior Developer",
             img:
                 "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/twitter.png",
             desc:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
         },
@@ -18,7 +17,6 @@ export default function Experience() {
             title: "Co-Founder of DELKA",
             img:
                 "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/youtube.png",
             desc:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
             featured: true,
@@ -29,7 +27,6 @@ export default function Experience() {
             title: "CEO of ALBI",
             img:
                 "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: "assets/linkedin.png",
             desc:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
         },
@@ -41,7 +38,7 @@ export default function Experience() {
                 {data.map((d) => (
                     <div className={d.featured ? "card featured" : "card"}>
                         <div className="top">
-                            <img src="assets/right-arrow.png" className="left" alt="" />
+                            <img src="" className="left" alt="" />
                             <img
                                 className="user"
                                 src={d.img}
